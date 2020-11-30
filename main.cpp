6adb9f5fec6964a3bda89d22eebbd1aa2e813648
lwh9346 <lwh9346@163.com>
2020-11-30 22:18:51 +0800

    实现了初步编译运行

--- main.cpp
+++ main.cpp
@@ -1,191 +1,8 @@
 #include <math.h>
 #include <random>
+#include <iostream>
 #define branchNum 4
-
-class treeNode
-{
-public:
-    int board[9][9]; //棋盘,1为己方,-1为对方,0为空
-    treeNode *parent;
-    treeNode *children[branchNum];
-    int childrenAction[branchNum][2];
-    int childrenCount; //子节点个数
-    int childrenCountMax;
-    int q;     //回溯后获胜局数
-    int n;     //访问次数
-    int depth; //节点深度
-    double score;
-    treeNode(int parentBoard[9][9], int action[2], int d)
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                board[i][j] = -parentBoard[i][j]; //双方反转
-            }
-        }
-        board[action[0]][action[1]] = -1;
-        q = 0;
-        n = 0;
-        depth = d;
-        childrenCount = 0;
-        evaluate();
-    }
-    treeNode *treePolicy()
-    {
-        if (childrenCountMax == 0)
-        {
-            return this;
-        }
-        if (childrenCountMax > childrenCount)
-        {
-            treeNode c(board, childrenAction[childrenCount], depth + 1);
-            childrenCount++;
-            return &c;
-        }
-        int bestChild = 0;
-        double maxScore = 0;
-        for (int i = 0; i < childrenCount; i++)
-        {
-            if (maxScore < children[i]->score)
-            {
-                bestChild = i;
-                maxScore = children[i]->score;
-            }
-        }
-        return children[bestChild]->treePolicy();
-    }
-    int simulation() //返回由当前节点开始模拟的结果
-    {
-        int simulationBoard[9][9];
-        int simulationBoardR[9][9]; //对方的棋盘
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                simulationBoard[i][j] = board[i][j];
-                simulationBoardR[i][j] = -board[i][j];
-            }
-        }
-        int r = result(simulationBoard);
-        int round = 0;
-        while (!r)
-        {
-            if (round % 2 == 0)
-            {
-                int *a = defaultPolicy(simulationBoard);
-                simulationBoard[a[0]][a[1]] = 1;
-                simulationBoardR[a[0]][a[1]] = -1;
-            }
-            else
-            {
-                int *a = defaultPolicy(simulationBoardR);
-                simulationBoard[a[0]][a[1]] = -1;
-                simulationBoardR[a[0]][a[1]] = 1;
-            }
-            r = result(simulationBoard);
-        }
-        return r;
-    }
-    void backup(int delta)
-    {
-        treeNode *node = this;
-        int d = 1;
-        while (node != nullptr)
-        {
-            if (delta && d % 2)
-            {
-                node->q++;
-            }
-            node->n++;
-            node->score = double(node->q) / double(node->n) + 0.707 * sqrt(double(node->depth) / double(node->n)); //得分公式
-            node = node->parent;
-            d++;
-        }
-    }
-
-private:
-    void evaluate()
-    {
-        //填充childrenAction、countMax
-        int result[9][9];
-        int resultTemp[9][9];
-        int boardR[9][9]; //对方视角的棋盘
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                boardR[i][j] = -board[i][j];
-            }
-        }
-        int validPositionCount = getValidPositions(board, result);
-        if (validPositionCount > branchNum)
-        {
-            childrenCountMax = branchNum;
-            int max[branchNum] = {1 << 31, 1 << 31, 1 << 31, 1 << 31};
-            int maxAction[branchNum][2];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (result[i][j])
-                    {
-                        board[i][j] = 1;
-                        boardR[i][j] = -1; //模拟一步
-                        int temp=getValidPositions(board,resultTemp)-getValidPositions(boardR,resultTemp);
-                        for (int k = 0; k < branchNum; k++)
-                        {
-                            if (temp>max[k])
-                            {
-                                max[k]=temp;
-                                maxAction[k][0]=i;
-                                maxAction[k][1]=j;
-                                break;
-                            }
-                            if (temp==max[k]&&rand()<5000)//六分之一的概率替换
-                            {
-                                maxAction[k][0]=i;
-                                maxAction[k][1]=j;
-                                break;
-                            }
-                            
-                        }
-                        board[i][j] = 0;
-                        boardR[i][j] = 0; //还原
-                    }
-                }
-            }
-            for (int i = 0; i < branchNum; i++)
-            {
-                childrenAction[i][0]=maxAction[i][0];
-                childrenAction[i][1]=maxAction[i][1];
-            }
-        }else{
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (result[i][j])
-                    {
-                        childrenAction[childrenCountMax][0]=i;
-                        childrenAction[childrenCountMax][1]=j;
-                        childrenCountMax++;
-                    }
-                }
-            }
-        }
-    }
-    int result(int board[9][9])
-    {
-        //0棋局未结束
-        //1棋局结束，当前方赢
-        //-1棋局结束，当前方输
-    }
-    int *defaultPolicy(int board[9][9])
-    {
-        //返回落子坐标
-    }
-};
+using namespace std;
 
 void gasFinding(int x, int y, int i, int j, int groupIndex, int board[9][9], char group[9][9], char groupGasState[81], char groupGasPosition[81][2], char positionState[9][9], char unicGasGroup[9][9])
 {
@@ -334,16 +151,272 @@ int getValidPositions(int board[9][9], int result[9][9])
     return r;
 }
 
+class treeNode
+{
+public:
+    int board[9][9]; //棋盘,1为己方,-1为对方,0为空
+    treeNode *parent;
+    treeNode *children[branchNum];
+    int childrenAction[branchNum][2];
+    int childrenCount; //子节点个数
+    int childrenCountMax;
+    int q;     //回溯后获胜局数
+    int n;     //访问次数
+    int depth; //节点深度
+    double score;
+    treeNode(int parentBoard[9][9], int action[2], int d,treeNode *p)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                board[i][j] = -parentBoard[i][j]; //双方反转
+            }
+        }
+        parent = p;
+        board[action[0]][action[1]] = -1;
+        q = 0;
+        n = 0;
+        depth = d;
+        childrenCount = 0;
+        evaluate();
+    }
+    treeNode *treePolicy()
+    {
+        if (childrenCountMax == 0)
+        {
+            return this;
+        }
+        if (childrenCountMax > childrenCount)
+        {
+            treeNode *c=new treeNode(board, childrenAction[childrenCount], depth + 1,this);
+            children[childrenCount]=c;
+            childrenCount++;
+            return c;
+        }
+        int bestChild = 0;
+        double maxScore = 0;
+        for (int i = 0; i < childrenCount; i++)
+        {
+            if (maxScore < children[i]->score)
+            {
+                bestChild = i;
+                maxScore = children[i]->score;
+            }
+        }
+        return children[bestChild]->treePolicy();
+    }
+    int simulation() //返回由当前节点开始模拟的结果
+    {
+        int simulationBoard[9][9];
+        int simulationBoardR[9][9]; //对方的棋盘
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                simulationBoard[i][j] = board[i][j];
+                simulationBoardR[i][j] = -board[i][j];
+            }
+        }
+        int r = result(simulationBoard);
+        int round = 0;
+        while (!r)
+        {
+            if (round % 2 == 0)
+            {
+                int a = defaultPolicy(simulationBoard);
+                simulationBoard[a/9][a%9] = 1;
+                simulationBoardR[a/9][a%9] = -1;
+            }
+            else
+            {
+                int a = defaultPolicy(simulationBoardR);
+                simulationBoard[a/9][a%9] = -1;
+                simulationBoardR[a/9][a%9] = 1;
+            }
+            r = result(simulationBoard);
+        }
+        return r;
+    }
+    void backup(int delta)
+    {
+        treeNode *node = this;
+        int d = 1;
+        while (node != nullptr)
+        {
+            if (delta && d % 2)
+            {
+                node->q++;
+            }
+            node->n++;
+            node->score = double(node->q) / double(node->n) + 0.707 * sqrt(double(node->depth) / double(node->n)); //得分公式
+            node = node->parent;
+            d++;
+        }
+    }
+
+private:
+    void evaluate()
+    {
+        //填充childrenAction、countMax
+        int result[9][9];
+        int resultTemp[9][9];
+        int boardR[9][9]; //对方视角的棋盘
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                boardR[i][j] = -board[i][j];
+            }
+        }
+        int validPositionCount = getValidPositions(board, result);
+        if (validPositionCount > branchNum)
+        {
+            childrenCountMax = branchNum;
+            int max[branchNum] = {1 << 31, 1 << 31, 1 << 31, 1 << 31};
+            int maxAction[branchNum][2];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (result[i][j])
+                    {
+                        board[i][j] = 1;
+                        boardR[i][j] = -1; //模拟一步
+                        int temp=getValidPositions(board,resultTemp)-getValidPositions(boardR,resultTemp);
+                        for (int k = 0; k < branchNum; k++)
+                        {
+                            if (temp>max[k])
+                            {
+                                max[k]=temp;
+                                maxAction[k][0]=i;
+                                maxAction[k][1]=j;
+                                break;
+                            }
+                            if (temp==max[k]&&rand()<5000)//六分之一的概率替换
+                            {
+                                maxAction[k][0]=i;
+                                maxAction[k][1]=j;
+                                break;
+                            }
+                            
+                        }
+                        board[i][j] = 0;
+                        boardR[i][j] = 0; //还原
+                    }
+                }
+            }
+            for (int i = 0; i < branchNum; i++)
+            {
+                childrenAction[i][0]=maxAction[i][0];
+                childrenAction[i][1]=maxAction[i][1];
+            }
+        }else{
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (result[i][j])
+                    {
+                        childrenAction[childrenCountMax][0]=i;
+                        childrenAction[childrenCountMax][1]=j;
+                        childrenCountMax++;
+                    }
+                }
+            }
+        }
+    }
+    int result(int board[9][9])
+    {
+        //0棋局未结束
+        //1棋局结束，当前方赢
+        //-1棋局结束，当前方输
+        int result [9][9];
+        int boardR[9][9]; //对方视角的棋盘
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                boardR[i][j] = -board[i][j];
+            }
+        }
+        int a,b;
+        a = getValidPositions(board,result);
+        b = getValidPositions(boardR,result);
+        if (a!=0&&b!=0)
+        {
+            return 0;
+        }
+        if (a==0)
+        {
+            return -1;
+        }
+        return 1;
+    }
+    int defaultPolicy(int board[9][9])
+    {
+        //返回落子坐标
+        int result[9][9];
+        int resultTemp[9][9];
+        int boardR[9][9]; //对方视角的棋盘
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                boardR[i][j] = -board[i][j];
+            }
+        }
+        int validPositionCount = getValidPositions(board, result);
+        int max = 1 << 31;
+        int maxAction[2];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (result[i][j])
+                {
+                    board[i][j] = 1;
+                    boardR[i][j] = -1; //模拟一步
+                    int temp = getValidPositions(board, resultTemp) - getValidPositions(boardR, resultTemp);
+                        if (temp > max)
+                        {
+                            max = temp;
+                            maxAction[0] = i;
+                            maxAction[1] = j;
+                            break;
+                        }
+                    board[i][j] = 0;
+                    boardR[i][j] = 0; //还原
+                }
+            }
+        }
+        return maxAction[0]*9+maxAction[1];
+    }
+};
+
 int main()
 {
-    int board[9][9] = {0};
+    srand(233);
+    int count = 0;
+    int board[9][9] = {
+        {0, 1, 1, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
     int action[2] = {2, 2};
-    treeNode root(board, action, 0);
-    while (true /*这里是停止搜索的条件*/)
+    treeNode root(board, action, 0,nullptr);
+    while (count<100 /*这里是停止搜索的条件*/)
     {
         treeNode *node = root.treePolicy();
         int result = node->simulation();
         node->backup(result == 1 ? 1 : 0);
+        count++;
     }
     int max = 0;
     int *bestAction = action;
@@ -355,4 +428,5 @@ int main()
             bestAction = root.childrenAction[i];
         }
     }
+    cout<<bestAction[0]<<" "<<bestAction[1]<<endl;
 }
